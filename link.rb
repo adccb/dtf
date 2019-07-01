e45fdea6499bf8e374b02b42b965137601edbcc9
autumn grace <the@autumn.zone>
2019-07-01 17:20:21 -0400

    finally fixed link.rb to dtrt

--- link.rb
+++ link.rb
@@ -1,16 +1,45 @@
 require "fileutils"
 
-fu = ENV['env'] ? FileUtils : FileUtils::DryRun
+fu = ENV["env"] == "go" ? FileUtils : FileUtils::DryRun
 
-# create all necessary dirs
-fu.mkdir_p '/Users/acrossan/.hammerspoon/'
-fu.mkdir_p '/Users/acrossan/.config/'
-fu.mkdir_p '/Users/acrossan/.vim/'
+dir_locations = [
+  "/Users/acrossan/.hammerspoon/",
+  "/Users/acrossan/.config/",
+  "/Users/acrossan/.config/karabiner"
+]
 
-# do all the goddamn symlinks
-fu.ln_sf '/Users/acrossan/.dtf/zshrc', '/Users/acrossan/.zshrc'
-fu.ln_sf '/Users/acrossan/.dtf/init.lua', '/Users/acrossan/.hammerspoon/init.lua'
-fu.ln_sf '/Users/acrossan/.dtf/nvim', '/Users/acrossan/.config/nvim'
-fu.ln_sf '/Users/acrossan/.dtf/gitignore_global', '/Users/acrossan/.gitignore_global'
-fu.ln_sf '/Users/acrossan/.dtf/karabiner.json', '/Users/acrossan/.config/karabiner/karabiner.json'
+symlink_locations = {
+  "/Users/acrossan/.dtf/zshrc" => "/Users/acrossan/.zshrc",
+  "/Users/acrossan/.dtf/init.lua" => "/Users/acrossan/.hammerspoon/init.lua",
+  "/Users/acrossan/.dtf/nvim" => "/Users/acrossan/.config/nvim",
+  "/Users/acrossan/.dtf/gitignore_global" => "/Users/acrossan/.gitignore_global",
+  "/Users/acrossan/.dtf/karabiner.json" => "/Users/acrossan/.config/karabiner/karabiner.json",
+}
+
+def remove_hostname (str) str.gsub "/Users/acrossan/", "" end
+
+if ENV["env"] != "go"
+  puts "NOTE: this run didn't do anything. if you want to\nactually change your system, run the following command:"
+  puts "\n\s\s\s\s$ env=go ruby link.rb\n\n\n"
+end
+
+puts "creating directories...\n\n"
+dir_locations.each do |fname|
+  if File.exist? fname
+    puts "#{remove_hostname fname} exists, skipping..."
+  else
+    puts "creating #{remove_hostname fname}..."
+    fu.mkdir_p fname
+  end
+end
+
+puts "\ncreating symlinks...\n\n"
+symlink_locations.each do |source, target|
+  if File.exist? target
+    puts "#{remove_hostname target} is already symlinked, skipping..."
+  else
+    puts "symlinking #{remove_hostname source} to #{remove_hostname target}"
+    fu.ln_sf source, target
+  end
+end
 
